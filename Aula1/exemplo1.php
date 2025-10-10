f31b9847388b98e96a5aa1624b4ed84f8c769668
José Augusto Gonçalves da Silva <jose.a.silva271@aluno.senai.br>
2025-10-10 16:32:51 -0300

    Introdução
    Introdução à linguagem PHP básica.

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,12 @@
+<?php
+// Geração de texto (string)
+    echo "Chama Sesi-Senai!";
+
+// Geração de texto estrutura (com tags e atributos)
+
+// Aspas simples dentro de aspas duplas
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+
+// Aspas duplas duplas dentro de aspas duplas
+    echo "<h2>Gerando <span class= \"destaque\">HTML</span> através do PHP.</h2>";
+?>
---
+++ Aula1/exemplo3.php
@@ -0,0 +1,13 @@
+<?php
+    echo            "Testando espaço em branco <br>";
+    echo "Testando espaço <br><br>";
+
+    // O caracter &nbsp; adiciona um espaço em branco dentro da string
+    echo "&nbsp; &#160;         Testando    espaço  em  branco";
+    echo
+    "Quebra de linha <br>";
+
+
+
+    echo "Quebra de linha <br>";
+?>
